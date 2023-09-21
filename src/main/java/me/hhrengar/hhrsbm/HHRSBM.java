fcf646d27e16f6b856e625220b9b3c5be7ee8a64
HHR <cetecihalilefe@gmail.com>
2023-09-21 19:18:07 +0300

    Testing

---
+++ src/main/java/me/hhrengar/hhrsbm/HHRSBM.java
@@ -0,0 +1,16 @@
+package me.hhrengar.hhrsbm;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+//asdasd
+public class HHRSBM implements ModInitializer {
+	public static final String MOD_ID ="hhrsbm";
+    public static final Logger LOGGER = LoggerFactory.getLogger("hhrsbm");
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/me/hhrengar/hhrsbm/HHRSBMClient.java
@@ -0,0 +1,10 @@
+package me.hhrengar.hhrsbm;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HHRSBMClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
