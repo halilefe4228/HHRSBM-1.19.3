814a28ab11ac7c4b98a68b05046b857dfb0ec9bf
HHR <cetecihalilefe@gmail.com>
2023-09-21 19:19:19 +0300

    Testing 2

--- src/main/java/me/hhrengar/hhrsbm/HHRSBM.java
+++ src/main/java/me/hhrengar/hhrsbm/HHRSBM.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 //asdasd
 public class HHRSBM implements ModInitializer {
+	//AAAA
 	public static final String MOD_ID ="hhrsbm";
     public static final Logger LOGGER = LoggerFactory.getLogger("hhrsbm");
 
